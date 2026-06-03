cf0e20c8bc91cba4ae70e3488413aa0a3fc167d1
Arthur <arthurbratigny@gmail.com>
2026-06-03 11:09:57 +0200

    logs

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,7 @@
 import { MongoClient, type Db } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
+const uri = process.env.MONGODB_URI;
+if (!uri) throw new Error("MONGODB_URI environment variable is not set");
 const dbName = process.env.MONGODB_DB_NAME || "wordle";
 
 const client = new MongoClient(uri);
